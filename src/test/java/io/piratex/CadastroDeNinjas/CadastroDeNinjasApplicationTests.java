e56247e983d6c15479ca6bdeb435b5742740408c
Morrango <152505873+Moranggoo@users.noreply.github.com>
2025-06-12 21:48:47 -0300

    first commit

---
+++ src/test/java/io/piratex/CadastroDeNinjas/CadastroDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package io.piratex.CadastroDeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

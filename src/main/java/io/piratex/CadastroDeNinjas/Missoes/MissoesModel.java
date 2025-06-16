8e40b68829b95d2dca287bacec049de20fb15f90
Morrango <152505873+Moranggoo@users.noreply.github.com>
2025-06-16 20:28:03 -0300

    feat: Cria uma entidade no banco de dados a partir do MissoesModel e NinjaModel

---
+++ src/main/java/io/piratex/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -0,0 +1,34 @@
+package io.piratex.CadastroDeNinjas.Missoes;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.piratex.CadastroDeNinjas.Ninja.NinjaModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+
+@Entity
+@Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MissoesModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    private String nome;
+
+    private String dificuldade;
+
+    // @OneToMany - Uma missao pode ter varios ninjas
+    @OneToMany(mappedBy = "missoes")
+    @JsonIgnore
+    private List<NinjaModel> ninjas;
+
+}
---
+++ src/main/java/io/piratex/CadastroDeNinjas/Ninja/NinjaModel.java
@@ -0,0 +1,41 @@
+package io.piratex.CadastroDeNinjas.Ninja;
+
+import io.piratex.CadastroDeNinjas.Missoes.MissoesModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// Entity transforma uma classe em entidade do banco de dados
+// JPA = Java Persistence API
+@Entity
+@Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(unique = true, name = "email")
+    private String email;
+
+    @Column(name = "img_url")
+    private String imgUrl;
+
+    @Column(name = "idade")
+    private int idade;
+
+    // @ManyToOne um ninja tem uma unica missao
+    @ManyToOne
+    @JoinColumn(name = "missoes_id") // Foreing Key ou chave estrangeira
+    private MissoesModel missoes;
+
+
+}

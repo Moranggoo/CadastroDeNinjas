e035ccfaf3747f4e2785b892eaacd8f0bbc89838
Morrango <152505873+Moranggoo@users.noreply.github.com>
2025-06-25 09:11:19 -0300

    feat: Atualiza o MissoesController com mock de dados

--- src/main/java/io/piratex/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/io/piratex/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,16 +1,34 @@
 package io.piratex.CadastroDeNinjas.Missoes;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/missoes")
 public class MissoesController {
 
     @GetMapping("/rotamissoes")
     public String rotaMissoes() {
         return "Primeira rota das missoes";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissoes() {
+        return "Missao criada";
+    }
+
+    @GetMapping("/listar")
+    public String listarMissoes() {
+        return "Missoes: ";
+    }
+
+    @PostMapping("/alterar")
+    public String alterarMissoes() {
+        return "Missao alterada";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissoes() {
+        return "Missao deletada";
     }
 
 }
